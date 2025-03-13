8c1743f63a10fed5dd9b0ec1f7eb50cbad6bb634
emmakturk <114186999+emmakturk@users.noreply.github.com>
2025-03-13 10:19:16 -0400

    Fix back button

--- TrailBlazer/TrailBlazer/View/AvailableRoutes.swift
+++ TrailBlazer/TrailBlazer/View/AvailableRoutes.swift
@@ -62,6 +62,7 @@ struct AvailableRoutesView: View {
             bottomTabBar
         }
         .onAppear { fetchDifficulties() }
+        .navigationBarBackButtonHidden(true)
     }
     
     // Bottom Navigation Bar
